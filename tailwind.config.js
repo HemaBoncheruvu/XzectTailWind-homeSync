b5345cfc8989128c318c123087dff2de4ec3e2be
HemaBoncheruvu <hemaboncheruvu13@gmail.com>
2024-07-06 10:54:38 +0530

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Dusc/index.html"],
+  theme: {
+    extend: {
+      colors:{
+      'gray':'#24365a'
+      }
+    },
+  },
+  plugins: [],
+}
+
